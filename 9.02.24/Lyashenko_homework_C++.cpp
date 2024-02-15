9856aaa88c05b40a2da1287820ced18076670a7d
RomanLyashenko <159914055+RomanLyashenko@users.noreply.github.com>
2024-02-15 13:03:49 +0300

    Add files via upload

---
+++ 9.02.24/Lyashenko_homework_C++.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "task1" << endl;
+    cout << "\tNever gonna give you up" << endl;
+    cout << "\tNever gonna let you down" << endl;
+    cout << "\tNever gonna run around and desert you" << endl;
+    cout << "\tNever gonna make you cry" << endl;
+    cout << "\tNever gonna say goodbye" << endl;
+    cout << "\tNever gonna tell a lie and hurt you" << endl;
+    cout << "\t\t\t\t-by Rick Astley" << endl;
+    cout << "task2" << endl;
+    cout << "Every" << endl;
+    cout << "\thunter" << endl;
+    cout << "\t\twants" << endl;
+    cout << "\t\t\tto know" << endl;
+    cout << "\t\t\t\twhere" << endl;
+    cout << "\t\t\t\t\tthe pheasant" << endl;
+    cout << "\t\t\t\t\t\t\tis" << endl;
+    cout << "task3" << endl;
+    cout << "\t\t THE GARAGE FOR SALE" << endl;
+    cout << "\t\t   5 square meters" << endl;
+    cout << "\t\t   Lyashenko Roman" << endl;
+    cout << "\t\tPhone:8 (800)-555-35-35" << endl;
+    cout << "\t  \t  8 \t  8\t  8" << endl;
+    cout << "\t\t(800)\t(800)\t(800)" << endl;
+    cout << "\t\t 555\t 555\t 555" << endl;
+    cout << "\t  \t 35 \t 35\t 35" << endl;
+    cout << "\t  \t 35 \t 35\t 35" << endl;
+}
+
