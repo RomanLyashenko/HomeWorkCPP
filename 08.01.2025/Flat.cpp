b74514b42d9e7fba491a49cd5f142939469366f8
RomanLyashenko <159914055+RomanLyashenko@users.noreply.github.com>
2025-01-08 17:29:06 +0300

    Add files via upload

--- 08.01.2025/Flat.cpp
+++ 08.01.2025/Flat.cpp
@@ -2,27 +2,40 @@
 
 Flat::Flat()
 {
+	this->humanNum = 2;
+	this->roomsNum = 2;
+	this->price = 1000;
+	this->humans = new Human[humanNum];
+	this->number = this->id++;
 }
 
-Flat::Flat(int size, int price)
+Flat::Flat(Human* humans, int price, int roomsNum, int humanNum)
 {
-	this->size = size;
+	this->humans = new Human[humanNum];
+	for (int i = 0; i < humanNum; i++) {
+		this->humans[i] = humans[i];
+	}
+	this->humanNum = humanNum;
+	this->roomsNum = roomsNum;
 	this->price = price;
+	this->number = this->id++;
 }
 
 Flat::Flat(Flat& flat)
 {
-	this->size = flat.size;
+	this->humans = new Human[flat.humanNum];
+	for (int i = 0; i < flat.humanNum; i++) {
+		this->humans[i] = flat.humans[i];
+	}
+	this->humanNum = flat.humanNum;
+	this->roomsNum = flat.roomsNum;
 	this->price = flat.price;
+	this->number = this->id++;
 }
 
-Flat::~Flat()
+Human* Flat::getHumans()
 {
-}
-
-int Flat::getSize()
-{
-	return this->size;
+	return this->humans;
 }
 
 int Flat::getPrice()
@@ -30,9 +43,25 @@ int Flat::getPrice()
 	return this->price;
 }
 
-void Flat::setSize(int size)
+int Flat::getRoomsNum()
 {
-	this->size = size;
+	return this->roomsNum;
+}
+
+int Flat::getHumanNum()
+{
+	return this->humanNum;
+}
+
+void Flat::setHumans(Human* humans, int humanNum)
+{
+	Human* buf = new Human[humanNum];
+	for (int i = 0; i < humanNum; i++) {
+		buf[i] = humans[i];
+	}
+	delete[] this->humans;
+	this->humans = buf;
+	this->humanNum = humanNum;
 }
 
 void Flat::setPrice(int price)
@@ -40,30 +69,47 @@ void Flat::setPrice(int price)
 	this->price = price;
 }
 
+void Flat::setRoomsNum(int roomsNum)
+{
+	this->roomsNum = roomsNum;
+}
+
+void Flat::setHumanNum(int humanNum)
+{
+	Human* buf = new Human[humanNum];
+	for (int i = 0; i < humanNum && i < this->humanNum; i++) {
+		buf[i] = this->humans[i];
+	}
+	delete[] this->humans;
+	this->humans = buf;
+	this->humanNum = humanNum;
+}
+
+void Flat::addHuman(Human human)
+{
+	Human* buf = new Human[humanNum + 1];
+	for (int i = 0; i < humanNum; i++) {
+		buf[i] = this->humans[i];
+	}
+	buf[humanNum] = human;
+	delete[] this->humans;
+	this->humans = buf;
+	this->humanNum++;
+}
+
 void Flat::print()
 {
-	cout << "Size: " << this->size << endl;
-	cout << "Price: " << this->price << "$" << endl;
-}
-
-bool Flat::operator==(Flat& flat)
-{
-	if (this->size == flat.size) {
-		return true;
+	cout << "Flat " << this->number << ":" << endl;
+	cout << "Price: " << this->price << endl;
+	cout << "Humans living: ";
+	for (int i = 0; i < this->humanNum; i++) {
+		cout << "Human " << i+1 << " ";
+		this->humans[i].print();
+		cout << endl;
 	}
-	return false;
 }
 
-void Flat::operator=(Flat& flat)
+Flat::~Flat()
 {
-	this->price = flat.price;
-	this->size = flat.size;
-}
-
-bool Flat::operator>(Flat& flat)
-{
-	if (this->price > flat.price) {
-		return true;
-	}
-	return false;
+	delete[] this->humans;
 }
--- 08.01.2025/Flat.h
+++ 08.01.2025/Flat.h
@@ -1,24 +1,32 @@
 #pragma once
 #include <iostream>
 using namespace std;
+#include <string>
+#include "Human.h"
 
 class Flat
 {
 private:
-	int size;
+	Human* humans;
 	int price;
+	int roomsNum;
+	int humanNum;
+	int number;
 public:
+	static int id;
 	Flat();
-	Flat(int, int);
+	Flat(Human*, int, int, int);
 	Flat(Flat&);
-	~Flat();
-	int getSize();
+	Human* getHumans();
 	int getPrice();
-	void setSize(int);
+	int getRoomsNum();
+	int getHumanNum();
+	void setHumans(Human*, int);
 	void setPrice(int);
+	void setRoomsNum(int);
+	void setHumanNum(int);
+	void addHuman(Human);
 	void print();
-	bool operator==(Flat&);
-	void operator=(Flat&);
-	bool operator>(Flat&);
+	~Flat();
 };
 
