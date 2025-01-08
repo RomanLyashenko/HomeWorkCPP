60b23ba1b9f67fa4330f96b88d505b112cb25e86
RomanLyashenko <159914055+RomanLyashenko@users.noreply.github.com>
2025-01-08 18:00:23 +0300

    Add files via upload

---
+++ Human/08.01.2025.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+#include "Human.h"
+#include "Date.h"
+#include "Lift.h"
+
+int main()
+{
+	Date date1(10, 10, 1964);
+	Human human1(1, "Oleg", "Oledovenko", "Alex", date1);
+	human1.countHumans();
+	human1.printHuman();
+	Lift lift(1, 10);
+	lift.liftOn();
+	lift.setCurrentFloor(5);
+	cout << lift.getCurrentFloor();
+}
