4d1b66ef3324ff1e14da27661423b450e82dd0a5
RomanLyashenko <159914055+RomanLyashenko@users.noreply.github.com>
2025-01-08 17:48:42 +0300

    Add files via upload

---
+++ fraction/08.01.2025.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include "Fraction.h"
+using namespace std;
+
+int main()
+{
+	Fraction obj1(3, 8);
+	Fraction obj2(4, 5);
+	obj1.printFraction();
+	cout << " ";
+	obj2.printFraction();
+	cout << endl;
+	obj1.sum(obj2);
+	cout << endl;
+	obj1.minus(obj2);
+	cout << endl;
+	obj1.multiplication(obj2);
+	cout << endl;
+	obj1.division(obj2);
+}
